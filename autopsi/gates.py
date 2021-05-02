ffb33f0c42f30897ae8934e0d371c9773ee88382
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-05-02 14:54:53 -0700

    :heavy_plus_sign: Add dtype as a Parameter for Gates

--- autopsi/gates.py
+++ autopsi/gates.py
@@ -5,9 +5,11 @@ class __GateTensor__:
     """Internal class that's subclassed to make each quantum gate.
 
     Args:
-        backend (module): Numpy-like Python library to use when compiling a gate to a tensor. (default numpy)"""
+        backend (module): Numpy-like Python library to use when compiling a gate to a tensor. (default numpy)
+        dtype (type): Data type to use for gates specified as matrices."""
 
     def __init__(self,
-                 backend=__np__
+                 backend=__np__,
+                 dtype=complex
                  ):
         self.backend = backend
