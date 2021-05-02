b8c27ed6ed8d3d99ece64ade0ba25f752125fec4
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-05-02 14:52:30 -0700

    :exclamation: Remove Returns: Summary from Class

--- autopsi/gates.py
+++ autopsi/gates.py
@@ -5,9 +5,7 @@ class __GateTensor__:
     """Internal class that's subclassed to make each quantum gate.
 
     Args:
-        backend (module): Numpy-like Python library to use when compiling a gate to a tensor. (default numpy)
-    Returns:
-        numpy.array: Tensor representing a quantum gate."""
+        backend (module): Numpy-like Python library to use when compiling a gate to a tensor. (default numpy)"""
 
     def __init__(self,
                  backend=__np__
