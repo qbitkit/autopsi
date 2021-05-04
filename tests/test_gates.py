2b9505b134ae91961baeaf32920566dc33c58a30
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-05-04 16:19:11 -0700

    :new: :clipboard: Testing: Add H Gate Test

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -1,5 +1,6 @@
 from unittest import TestCase as __tc__
 from autopsi import gates as __gates__
+import numpy as __np__
 
 
 class TestInternalFunctions(__tc__):
@@ -8,3 +9,12 @@ class TestInternalFunctions(__tc__):
         testing_gatetensor = __gates__.__GateTensor__(backend=testing_message)
         self.assertEqual(testing_message,
                          testing_gatetensor.backend)
+
+class TestSingleQubitGates(__tc__):
+    def test_h(self):
+        h_list = [[(0.7071067811865475+0j), (0.7071067811865475+0j)],
+                  [(0.7071067811865475+0j), (-0.7071067811865475+0j)]]
+        self.assertEqual(
+            h_list,
+            __gates__.h().tensor().tolist()
+        )
