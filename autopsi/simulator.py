9391a337f9fb8a35202a862cae5d9af5abf3a447
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-05-05 15:51:39 -0700

    :1234:  Change Order of Functions

--- autopsi/simulator.py
+++ autopsi/simulator.py
@@ -11,6 +11,7 @@ class Tensor:
         device (str): String specifying a device to use when multiplying matrices. (default None)
         entrypoint (list): If specified, simulator will start out with the specified value as its state. (default None)
         trace (bool): If set to True, the simulator's state will be saved in memory prior to each quantum gate's execution. (default True)"""
+
     def __init__(self,
                  backend=__np__,
                  dtype=complex,
@@ -28,64 +29,6 @@ class Tensor:
 
         self.state = tensor(entrypoint if entrypoint is not None else [1, 0],
                             dtype=self.dtype)
-
-    def step(self,
-             tensor=None):
-        """Alter the simulator's state by applying a quantum logic gate specified as a tensor.
-
-        Args:
-            tensor (list): If specified, the value of the simulator will be multiplied by the specified tensor."""
-        product = self.backend.matmul
-
-        if tensor is not None:
-
-            if self.trace is True:
-                self.history.append(self.state)
-
-            if self.device is None:
-                self.state = product(self.state,
-                                     tensor)
-
-            if self.device is not None:
-                with __tf__.device(self.device):
-                    self.state = product(self.state,
-                                         tensor)
-
-    def amplitudes(self):
-        """Returns the current state of the simulator as amplitudes.
-
-        Returns:
-            numpy.ndarray: Current state of the simulator, as amplitudes."""
-        return self.state
-
-    def probabilities(self):
-        """Return the state of the simulator as probabilities.
-        This function works by squaring the state of the simulator stored as amplitudes.
-
-        Returns:
-            numpy.ndarray: Current state of the simulator, as probabilities"""
-        abs = self.backend.abs
-        return abs(self.state) ** 2
-
-    def history(self):
-        """If the simulator has tracing enabled, this function will return the history of the simulator's state.
-
-        Returns:
-            list(numpy.ndarray): Timeline of the simulator's state prior to applying each gate."""
-
-        if self.history:
-            return self.history
-
-    def batch_step(self,
-                   tensors=None):
-        """Alter the simulator's state by taking the tensor product of the simulator's state and the specified batch of tensors.
-
-        Args:
-            tensors(numpy.ndarray): List of tensors to multiply by the simulator's state. (default None)"""
-
-        if tensors is not None:
-            for i in tensors:
-                self.step(tensor=i)
 
     def astype(self,
                dtype=float,
@@ -111,6 +54,31 @@ class Tensor:
                [item.astype(dtype) # Convert each variable to the specified Data Type.
                 for item in array_to_copy], # Iterate over the array we want to copy.
                dtype=dtype) # Ensure the array's Data Type gets specified with what we just cast it to.
+
+    def amplitudes(self):
+        """Returns the current state of the simulator as amplitudes.
+
+        Returns:
+            numpy.ndarray: Current state of the simulator, as amplitudes."""
+        return self.state
+
+    def probabilities(self):
+        """Return the state of the simulator as probabilities.
+        This function works by squaring the state of the simulator stored as amplitudes.
+
+        Returns:
+            numpy.ndarray: Current state of the simulator, as probabilities"""
+        abs = self.backend.abs
+        return abs(self.state) ** 2
+
+    def history(self):
+        """If the simulator has tracing enabled, this function will return the history of the simulator's state.
+
+        Returns:
+            list(numpy.ndarray): Timeline of the simulator's state prior to applying each gate."""
+
+        if self.history:
+            return self.history
 
     def measure(self,
                 return_as_type=int):
@@ -149,3 +117,36 @@ class Tensor:
 
         return [self.measure(dtype) # Take a measurement each iteration and append it to the list we return
                 for shot in range(shots)] # Iterate over the number of shots we need to take represented as a range object
+
+    def step(self,
+             tensor=None):
+        """Alter the simulator's state by applying a quantum logic gate specified as a tensor.
+
+        Args:
+            tensor (list): If specified, the value of the simulator will be multiplied by the specified tensor."""
+        product = self.backend.matmul
+
+        if tensor is not None:
+
+            if self.trace is True:
+                self.history.append(self.state)
+
+            if self.device is None:
+                self.state = product(self.state,
+                                     tensor)
+
+            if self.device is not None:
+                with __tf__.device(self.device):
+                    self.state = product(self.state,
+                                         tensor)
+
+    def batch_step(self,
+                   tensors=None):
+        """Alter the simulator's state by taking the tensor product of the simulator's state and the specified batch of tensors.
+
+        Args:
+            tensors(numpy.ndarray): List of tensors to multiply by the simulator's state. (default None)"""
+
+        if tensors is not None:
+            for i in tensors:
+                self.step(tensor=i)
